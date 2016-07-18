6b7697849efbf00af280846f06a467588a1773dd
Malcolm Brooks <brooksofmaine@gmail.com>
2016-07-18 10:34:26 +0000

    add kickstarter day count to php-home.html and update home image and dig dates

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   var ho = 'pfp-home';
   $('#content').load('./content/html/' + ho + '.html');
   $('#pfp-home').addClass('active'); //show we're at home
@@ -17,3 +18,39 @@ $(document).ready(function() {
     return false;
   });
 });
+
+$(document).load(function () {
+ // code here
+ // window.onload = function()
+//{
+ document.getElementById("y").textContent = "one third of the time";
+ });     
+ 
+ /* var daysDiff = function() {
+  //Get 1 day in milliseconds
+  var one_day=1000*60*60*24;
+  // Convert both dates to milliseconds
+  var endDate = new Date(2016, 07, 13, 0, 0, 0, 0);
+  console.log("endDate = " + endDate);
+  var today = new Date();
+console.log("today = " + today);
+  // Calculate the difference in milliseconds
+  var difference_ms = endDate - today;
+   console.log("difference_ms = " + difference_ms); 
+  // Convert back to days and return
+ var difference_ds = Math.round(difference_ms/one_day); 
+    console.log("difference_ds = " + difference_ds); 
+  return difference_ds;
+}
+
+//date counter for kickstarter
+console.log("DOM is super ready");
+var daysRemaining = String(daysDiff());
+console.log("daysleft now = " + daysRemaining);
+//document.getElementById("x").innerHTML = daysRemaining;
+
+  */
+  
+  
+
+
